57d90baec24ae473ad0191dc4c7cdb4d8dfbba3d
PeterGeers <peter@pgeers.nl>
2026-02-18 14:43:55 +0100

    Fix TypeScript type definitions for i18n - use simpler approach

---
+++ frontend/src/react-i18next.d.ts
@@ -0,0 +1,18 @@
+// TypeScript definitions for react-i18next
+import 'react-i18next';
+
+declare module 'react-i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'common';
+    resources: {
+      common: any;
+      auth: any;
+      reports: any;
+      str: any;
+      banking: any;
+      admin: any;
+      errors: any;
+      validation: any;
+    };
+  }
+}
